51ef2bb546cf27582d7393afa82a610a33d44226
Kevin McGill <kevin.mcgill@mable.com.au>
2022-02-21 22:49:30 +1100

    Added bufferline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -48,6 +48,7 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim" -- Easily comment stuff
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
+  use "akinsho/bufferline.nvim"
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
