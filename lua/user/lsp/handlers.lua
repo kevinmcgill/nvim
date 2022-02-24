981499f98bdf1e8f0fa03347ed7db1b6e1aa706c
Kevin McGill <kevmcgill@gmail.com>
2022-02-24 14:56:43 +1100

    Allow json files to be formatted by Prettier

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -82,7 +82,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" or client.name == "solargraph" then
+  if client.name == "tsserver" or client.name == "solargraph" or client.name == "jsonls" then
     client.resolved_capabilities.document_formatting = false
   end
   lsp_keymaps(bufnr)
