b0daca93f8a0ac62c2fa43c55fcdb3b36eab1771
Kevin McGill <kevmcgill@gmail.com>
2022-02-24 22:56:55 +1100

    Add lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,6 +49,7 @@ return packer.startup(function(use)
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
   use "akinsho/bufferline.nvim"
+  use 'nvim-lualine/lualine.nvim'
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
