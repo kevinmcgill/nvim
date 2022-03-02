9e5c3f2d2aa5d6ac77edf8409d13e97ec4beb2e4
Kevin McGill <kevmcgill@gmail.com>
2022-03-02 22:45:03 +1100

    Cleanup nvim-tree config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -35,17 +35,7 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
   disable_netrw = true,
-  hijack_netrw = true,
   open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_close = true,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -60,30 +50,17 @@ nvim_tree.setup {
     update_cwd = true,
     ignore_list = {},
   },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
   view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    auto_resize = true,
+    width = 40,
+    preserve_window_proportions = true,
     mappings = {
-      custom_only = false,
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
       },
     },
-    number = false,
-    relativenumber = false,
   },
-  quit_on_open = 0,
-  git_hl = 1,
   disable_window_picker = 0,
   root_folder_modifier = ":t",
   show_icons = {
