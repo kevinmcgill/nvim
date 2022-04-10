4e7071dc9970ac29fcdf90873ec2025820c32009
Kevin McGill <kevmcgill@gmail.com>
2022-04-10 20:52:31 +1000

    bufferline and toggleterm now use `main`

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -48,9 +48,9 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim" -- Easily comment stuff
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
-  use "akinsho/bufferline.nvim"
+  use { "akinsho/bufferline.nvim", branch = 'main' }
   use 'nvim-lualine/lualine.nvim'
-  use "akinsho/toggleterm.nvim"
+  use { "akinsho/toggleterm.nvim", branch = 'main' }
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
