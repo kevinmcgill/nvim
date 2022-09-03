d56ed445c3476b15247210e2f6cc9cb5eb897a92
Kevin McGill <kevmcgill@gmail.com>
2022-09-03 22:56:31 +1000

    Show code context as a sticky header

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,6 +91,7 @@ return packer.startup(function(use)
   }
   use "p00f/nvim-ts-rainbow"
   use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use 'nvim-treesitter/nvim-treesitter-context'
 
   -- Git
   use "lewis6991/gitsigns.nvim"
